904a2a6e6fbbc237ea5081ab0eefa212bf7280e4
carmen_polo_mackbook <grafica.carmen.polo@gmail.com>
2019-01-31 11:01:03 -0300

    menú hamburgueda fallido:(

---
+++ hamburguesa/hamburguesa.js
@@ -0,0 +1,19 @@
+// Menú bar
+
+$(function(){
+  var pull = $('#pull');
+      menu = $('nav ul');
+      menuHeight = menu.height();
+
+      $(pull).on('click',function(e) {
+        e.preventDefault();
+        menu.slideTooggle();
+      });
+
+      $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')){
+          menu.removeAttr('style');
+        }
+      });
+});
