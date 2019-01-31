7f5dbf7ac844cfb0476fd8a5846e31b086ca95c8
carmen_polo_mackbook <grafica.carmen.polo@gmail.com>
2019-01-31 13:04:15 -0300

    todo fallido

---
+++ brokerage-prueba/wp-content/themes/wp_boilerplate-master/page-nosotros.php
@@ -0,0 +1,36 @@
+<?php get_header() ?>
+
+<!--=============================
+              Vimeo
+=============================-->
+
+<div class="container vimeo">
+<!-- 16:9 aspect ratio -->
+<div class="embed-responsive embed-responsive-16by9">
+<iframe src="https://player.vimeo.com/video/255684872?color=389389&title=0&byline=0&portrait=0"
+  width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen
+  class="embed-responsive-item">
+</iframe>
+</div>
+</div>
+
+<!--=============================
+        datos
+=============================-->
+
+<div class="bajada">
+BROKERAGE·CHILE es una Empresa integrada por profesionales, técnicos y especialistas, con una vasta experiencia por más de
+17 años en la gestión eficiente de las instalaciones que administra. Cuya característica principal
+es <em>“brindar una gestión personalizada, de calidad, eficiencia y cercanía con los Residentes y Usuarios”.</em>
+</div>
+<div class="sub_bajada">
+Estamos ubicados en Av. La Dehesa 1844, Of. 804. Lo Barnechea.<br>
+<i class="fas fa-phone"></i> +56 (2) 2942 5590  <i class="far fa-envelope"></i> contacto@inspira.cl
+</div>
+
+<!--=============================
+              FOOTER
+=============================-->
+
+
+<?php get_footer() ?>
